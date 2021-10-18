f931727d1fa8f8cf040a2c256da203da6c396e11
FalconLeandro <cronosteo1034@gmail.com>
2021-10-17 23:30:27 -0300

    ´a´

---
+++ dist/js/animado.js
@@ -0,0 +1,12 @@
+let animado = document.querySelectorAll('.animado');
+function mostrarScroll() {
+    let scrollTop = document.documentElement.scrollTop;
+    for (var i = 0; i < animado.length; i++) {
+        let alturaAnimado = animado[i].offsetTop;
+        if (alturaAnimado<scrollTop) {
+            animado[i].style.opacity = 1;
+        }
+        
+    }
+}
+window.addEventListener('scroll', mostrarScroll);
---
+++ dist/js/carrusel.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $('.carousel').owlCarousel({
+        margin:20,
+        loop:true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:false
+            }
+        }
+        
+
+    });
+});
