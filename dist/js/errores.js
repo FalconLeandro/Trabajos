6b63c637d337f257984780375e30bd3046ccd234
Leandro Falcon <cronosteo1034gmail.com>
2021-10-19 22:18:22 -0300

    commit para la hincha bolas demica

---
+++ dist/js/errores.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function(){
+    let formulario = document.querySelector(".col")
+    formulario.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        let errores = [];
+
+        let campoNombre = document
+    })
+}
