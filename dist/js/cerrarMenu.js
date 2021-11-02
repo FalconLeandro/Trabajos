f8f07a79bcd6eccc2f8ac6268f826af6aa1ffd13
Leandro Falcon <cronosteo1034gmail.com>
2021-11-02 00:45:10 -0300

    responsive6 y estilos a proyectos

--- dist/js/cerrarMenu.js
+++ dist/js/cerrarMenu.js
@@ -1,8 +1,9 @@
-let links = document.querySelectorAll('.nav-link');
 
-links.addEventListener('click', function() {
-    
-    console.log('ggggglinks');
-    
-    
-});
+  const btn=document.querySelector('.menu-btn');
+  const links = document.querySelectorAll(".nav-link a");
+  function doit() {
+    btn.checked = false;
+  }
+  links.forEach(cbox =>{cbox.addEventListener("click", doit);})
+
+  
