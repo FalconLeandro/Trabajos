03b3fdd2ee4b0a75169c03a62a95e50f03b68bb1
Leandro Falcon <cronosteo1034@gmail.com>
2023-08-04 04:06:21 -0300

    social position mobile

--- dist/js/main.js
+++ dist/js/main.js
@@ -22,16 +22,37 @@ window.addEventListener("scroll", () => {
 // });
 const social = document.querySelector(".social");
 window.addEventListener("scroll", () => {
+
+  var mediaQSocial = window.matchMedia("(min-width: 760px)");
   const scrollPos = window.scrollY;
-  if (scrollPos > 10 && scrollPos < 3000 ) {
-    social.style.transform = "translate(207px)";
-    social.style.transition = "1s";
-    social.style.opacity = "1";
-    (social.style.transitionProperty = "transform"), "opacity";
+  if (mediaQSocial.matches) {
+    console.log('mayor a 780px');
+    if (scrollPos > 10 && scrollPos < 3000 ) {
+      social.style.transform = "translate(207px)";
+      social.style.transition = "1s";
+      social.style.opacity = "1";
+      (social.style.transitionProperty = "transform"), "opacity";
+    } else {
+      // social.classList.add("social-footer");
+      social.style.transform = "translate(250px)";
+      social.style.transition = "1s";
+      social.style.transitionProperty = "transform";
+    }
+    
   } else {
-    // social.classList.add("social-footer");
-    social.style.transform = "translate(250px)";
-    social.style.transition = "1s";
-    social.style.transitionProperty = "transform";
+    console.log('menor a 780px');
+
+    if (scrollPos > 10 && scrollPos < 2500 ) {
+      social.style.transform = "translateY(-60px)";
+      social.style.transition = "1s";
+      social.style.opacity = "1";
+      (social.style.transitionProperty = "transform"), "opacity";
+    } else {
+      // social.classList.add("social-footer");
+      social.style.transform = "translateY(0px)";
+      social.style.transition = "1s";
+      social.style.transitionProperty = "transform";
+    }
   }
+  
 });
