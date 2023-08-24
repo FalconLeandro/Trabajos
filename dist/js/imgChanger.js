33fed2c445a0a1f72e91a91bf861bc825a2569d9
Leandro Falcon <cronosteo1034@gmail.com>
2023-08-24 07:51:27 -0300

    mediaquery fotoperfil appear

--- dist/js/imgChanger.js
+++ dist/js/imgChanger.js
@@ -1,14 +1,17 @@
 // var arreglo=['dist/img/fotoPerfil2bis.jpg','dist/img/fotoPerfil2.png','dist/img/fotoPerfil3.jpg']
 // var i = 0;
 
-// document.getElementById('fotoPerfil').addEventListener("click", function( event ) {
-    
-//     document.getElementById('fotoPerfil').src=arreglo[i];
-//     i++;
-//     if (i==3) {
-//       i=0;
-//     }
-    
-//     }, false);
+var fotoPerfil = document.getElementById("img");
+console.log(fotoPerfil);
+var mQ = window.matchMedia("(min-width: 1024px)");
+
+if (mQ.matches) {
+    console.log(mQ.matches);
+    fotoPerfil.innerHTML= `<img id="fotoPerfil" src="dist/img/fotoPerfil2bis.jpg" alt="fotoPerfil" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-offset="400" data-aos-duration="500">`;
+} else {
+    fotoPerfil.innerHTML= `<img id="fotoPerfil" src="dist/img/fotoPerfil2bis.jpg" alt="fotoPerfil" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-offset="200" data-aos-duration="500"></img>`
+}
+
+
 
   
